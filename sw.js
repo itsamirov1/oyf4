0d2e86698ad86df8a09c2c392ba6f2be20bbaa31
Muhammad-Ali <151931436+itsamirov1@users.noreply.github.com>
2026-03-24 22:07:07 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='oyf-v6';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','index.html','manifest.json'])));self.skipWaiting()});
+const CACHE='oyf-v7';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request)))});
